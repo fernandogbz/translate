456f00fde1f5d94bd597243ff5d3ca346a0c5924
Fernando Jimenez <fgjv06@gmail.com>
2023-05-17 17:01:12 -0400

    JS - all different option countries saved in let option

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ const selectTag = document.querySelectorAll("select");
 selectTag.forEach(tag => {
   for (const countryCode in countries) {
     console.log(countries[countryCode]);
-
+    let option = `<option value="${countryCode}">${countries[countryCode]}</option>`;
   }
 })
